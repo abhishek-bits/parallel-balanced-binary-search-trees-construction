670a9aedc4cb68850cebdc569523c8f659b53958
abhishek-bits <h20211030070@hyderabad.bits-pilani.ac.in>
2022-07-05 02:18:33 +0530

    override toString()

--- src/BinaryTreeNode.java
+++ src/BinaryTreeNode.java
@@ -19,11 +19,28 @@ public class BinaryTreeNode {
 		return data;
 	}
 	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
 	public BinaryTreeNode getLeft() {
 		return left;
 	}
 	
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	
 	public BinaryTreeNode getRight() {
 		return right;
+	}
+	
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+	
+	@Override
+	public String toString() {
+		return "["+data+"]";
 	}
 }
